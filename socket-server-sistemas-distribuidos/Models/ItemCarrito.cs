30e6319f4c8ecda976b422859325e0681f8ae98c
Sky <xpolargeist007x@gmail.com>
2026-05-19 19:03:27 -0600

    Código autogenerado

---
+++ socket-server-sistemas-distribuidos/Models/ItemCarrito.cs
@@ -0,0 +1,8 @@
+﻿namespace socket_server_sistemas_distribuidos.Models
+{
+    public class ItemCarrito
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ socket-server-sistemas-distribuidos/Models/SolicitudCompra.cs
@@ -0,0 +1,9 @@
+﻿namespace socket_server_sistemas_distribuidos.Models
+{
+    public class SolicitudCompra
+    {
+        public string Accion { get; set; } = "COMPRAR";
+        public string CorreoCliente { get; set; } = string.Empty;
+        public List<ItemCarrito> Items { get; set; } = new();
+    }
+}
